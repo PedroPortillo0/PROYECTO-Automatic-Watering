8ebb2a8fb097a96ce1f195f794c5562f97568fc3
PedroPortillo0 <pedro.portillo.r22@gmail.com>
2023-07-26 02:31:32 -0600

    [UPDATE] informacion plantas

--- src/componentes/Plantilas/InformacionPlanta.jsx
+++ src/componentes/Plantilas/InformacionPlanta.jsx
@@ -1,29 +1,34 @@
 import { Carrusel } from "../organismo/Carrosul";
 import { Principal } from "../atomos/TituloPage";
-
+import "../../estilos/InfoPlantas.css";
 
 function InformacionPlantas() {
     return ( 
         <>
             {/* carrusel y bienvenida*/}
-            <div className="text-gray-100 pt-5 fondo ">
-    
+            <div className="text-gray-100 pt-9 fondo  ">
                     <Principal titulo = "Bienvenido"/>
-           
                 <div>
                     <Carrusel/>
                 </div>
             </div>
-            {/* informacion de la planta*/}
-            <div>
-                
+            
+            <div className="padre1"> {/*div padre*/}
+                {/* informacion de la planta*/}
+                <div className="hijo2"> 
+                    <h1>hola</h1>
+                    <h1>hola</h1>
+                    <h1>hola</h1>
+                    <h1>hola</h1>
+                </div>
+                <div className="hijo3">
+                    <h1>hola</h1>
+                    <h1>hola</h1>
+                    <h1>hola</h1>
+                    <h1>hola</h1>
+                </div>
             </div>
-            {/* pie de pagina*/}
-            <div>
-
-            </div> 
         </>
     );
 }
-
 export default InformacionPlantas;
